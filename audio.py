b7da48c30fc0320884ac93f0fe6ac93cfbad7560
Harriet Crisp <harrietcrisp@Harriets-Air.home>
2020-05-11 23:13:25 +0100

    functions to record and play audio files

---
+++ audio.py
@@ -0,0 +1,94 @@
+import pyaudio
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def play_audio(filename):
+    # Set chunk size of 1024 samples per data frame
+    chunk = 1024  
+
+    # Open the sound file 
+    wf = wave.open(filename, 'rb')
+
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
+
+    # Open a .Stream object to write the WAV file to
+    # 'output = True' indicates that the sound will be played rather than recorded
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+
+    # Read data in chunks
+    data = wf.readframes(chunk)
+
+    # Play the sound by writing the audio data to the stream
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    # Close and terminate the stream
+    stream.close()
+    p.terminate()
+
+
+
+def record_audio(filename):
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def plot_audio(filename):
+
+    spf = wave.open(filename, "r")
+
+    # Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, "Int16")
+
+    plt.figure(1)
+    plt.title("Audio Signal")
+    plt.plot(signal)
+    plt.show()
+
+record_audio('recorded.wav')
+play_audio('recorded.wav')
+plot_audio('recorded.wav')
