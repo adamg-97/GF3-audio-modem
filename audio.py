a7e5bb597a0fc6b8a4ff87bcc218e3ad8a239312
Adam Goldney <atg31@cam.ac.uk>
2020-05-14 11:47:56 +0100

    Updated audio.py and Audio

--- audio.py
+++ audio.py
@@ -27,6 +27,15 @@ sd.default.channels = 1
 # fs, signal = scipy.io.wavfile.read('filename')
 # scipy.io.wavfile.write(r"filename", fs, myarray)
 
+def play_record(data, fs, padding_before=0.5, padding_after=0):
+
+    data_padded = np.pad(data, (int(padding_before*fs), int(padding_after*fs)), 'constant', constant_values=0)
+    print("Recording...")
+    signal = sd.playrec(data_padded, fs)
+    sd.wait()
+    print("Finished recording")
+    return signal[:,0]
+
 def plot_audio(filename):
 
     # Extract Raw Audio from Wav File
