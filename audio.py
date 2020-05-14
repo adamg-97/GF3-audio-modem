781a6ae4d56c9c551f2626628cd2b171d9885437
Adam Goldney <atg31@cam.ac.uk>
2020-05-14 10:57:25 +0100

    Updated audio file

--- audio.py
+++ audio.py
@@ -1,88 +1,36 @@
-import pyaudio
-import wave
 import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = [16,9]
+
+
 import numpy as np
 import sys
 import scipy.io.wavfile
 from scipy.signal import chirp, spectrogram, convolve
 from IPython.display import Audio
 import sounddevice as sd
+sd.default.channels = 1
 
 
-def play_audio(filename):
-    chunk = 1024   # Set chunk size of 1024 samples per data frame
+# Record audio using:
+# import sounddevice as sp
 
-    wf = wave.open(filename, 'rb')   # Open the sound file 
+# myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
 
-    p = pyaudio.PyAudio()   # Create an interface to PortAudio
+# Play and record simultaneously:
+# myrecording = sd.playrec(myarray, fs, channels=1)
 
-    # Open a .Stream object to write the WAV file to
-    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                    channels = wf.getnchannels(),
-                    rate = wf.getframerate(),
-                    output = True)
-
-    data = wf.readframes(chunk)    # Read data in chunks
-
-    # Play the sound by writing the audio data to the stream
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(chunk)
-
-    # Close and terminate the stream
-    stream.close()
-    p.terminate()
+# Playback:
+# sd.play(myarray, fs)
 
 
-
-def record_audio(filename, time, fs):
-    chunk = 1024 
-    sample_format = pyaudio.paInt16  # 16 bits per sample
-    channels = 1
-    fs = fs  # Record at fs samples per second
-    seconds = time
-
-    p = pyaudio.PyAudio()
-
-    print('Recording')
-
-    stream = p.open(format=sample_format,
-                    channels=channels,
-                    rate=fs,
-                    frames_per_buffer=chunk,
-                    input=True)
-
-    frames = []  # Initialize array to store frames
-
-    # Store data in chunks for 3 seconds
-    for i in range(0, int(fs / chunk * seconds)):
-        data = stream.read(chunk)
-        frames.append(data)
-
-    stream.stop_stream()
-    stream.close()
-
-    # Terminate the PortAudio interface
-    p.terminate()
-
-    print('Finished recording')
-
-    # Save the recorded data as a WAV file
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+# Read/Write audio to file using
+# fs, signal = scipy.io.wavfile.read('filename')
+# scipy.io.wavfile.write(r"filename", fs, myarray)
 
 def plot_audio(filename):
 
-    spf = wave.open(filename, "r")
-
     # Extract Raw Audio from Wav File
-    signal = spf.readframes(-1)
-    signal = np.fromstring(signal, "Int16")
-    fs = spf.getframerate()
+    fs, signal = scipy.io.wavfile.read(filename)
 
     time = np.linspace(0, len(signal) / fs, num=len(signal))
 
@@ -103,15 +51,6 @@ def plot_audio(filename):
 
     return
 
-def get_audio(filename):
-    spf = wave.open(filename, "r")
-
-    # Extract Raw Audio from Wav File
-    signal = spf.readframes(-1)
-    signal = np.fromstring(signal, "Int16")
-    fs = spf.getframerate()
-    
-    return signal, fs
 
 def get_peaks(signal, thresh):
     peaks = []
